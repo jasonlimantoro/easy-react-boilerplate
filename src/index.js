b6ac6ef4b5802f06c18c57d14abe5ca640bc8cf4
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-07-26 20:43:43 +0800

    feat: basic prompt

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const inquirer = require("inquirer");
+const fs = require("fs-extra");
+const path = require("path");
+
+const templatesDir = path.join(__dirname, "../templates");
+
+const copyTemplate = (answers) => {
+	const templateDir = `${templatesDir}/${answers.template}`;
+	if (!fs.existsSync(answers.appName)) {
+		fs.mkdirsSync(answers.appName);
+	}
+	fs.copySync(templateDir, answers.appName, {
+		filter: function (src) {
+			return !/(node_modules|dist)/.test(src);
+		},
+	});
+};
+
+inquirer
+	.prompt([
+		{ name: "appName", message: "Enter app name", default: "app" },
+		{
+			name: "template",
+			type: "list",
+			message: "Choose a template",
+			choices: fs.readdirSync(templatesDir),
+		},
+		{
+			name: "confirmation",
+			type: "confirm",
+			message: "Are you sure?",
+		},
+	])
+	.then((answers) => {
+		copyTemplate(answers);
+		console.log("Copied to", answers.appName, "directory!");
+		console.log("Done.");
+	});
